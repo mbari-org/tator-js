789391883f47b0b0a91242914694fc5f033a3094
danellecline <dcline@mbari.org>
2025-12-31 08:27:13 -0800

    added CORS and removed outdated README links

--- src/annotator/guiFPS.js
+++
@@ -1,8 +0,0 @@
-
-// Constrain the video display FPS to not allow dropped frames during playback
-//
-export var guiFPS = 60;
-
-export function setGuiFPS(fps) {
-  guiFPS = fps;
-}
--- test/test-localization.spec.js
+++
@@ -1,5 +0,0 @@
-const { test, expect } = require('./fixtures.js');
-
-test('Test localization functionality', async ({ organization }) => {
-  console.log('blah');
-});
